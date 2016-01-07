0d119614ad8682c0b00eb5150fbddad391545bdb
shixz <shixz@zhongsou.com>
2016-01-07 10:23:08 +0800

    添加读取WAL日志工具

--- src/tools/wal_reader.cpp
+++ src/tools/wal_reader.cpp
@@ -220,7 +220,7 @@ void parse_data(const std::string& data)
     parse_item_data(data);
   else if (FLAGS_type == "user") 
     parse_user_data(data);
-  else if (FALGS_type == "click")
+  else if (FLAGS_type == "click")
     parse_click_data(data);
 }
 
